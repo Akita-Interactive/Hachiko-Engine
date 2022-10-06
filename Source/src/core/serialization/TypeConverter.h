fb5e7892a916fda91271c98f651ea923fc1a7e45
Alvaro Soppelsa <alvarosoppelsa@gmail.com>
2022-10-06 16:13:47 +0200

    Serialize particle properties

--- Source/src/core/serialization/TypeConverter.h
+++ Source/src/core/serialization/TypeConverter.h
@@ -446,7 +446,7 @@ namespace YAML
 
         static bool decode(const Node& node, Hachiko::ParticleSystem::Emitter::Properties& rhs)
         {
-            if (!node.IsSequence() || node.size() != 8) // TODO: After save the scene change to 9
+            if (!node.IsSequence() || node.size() != 9)
             {
                 return false;
             }
@@ -459,7 +459,7 @@ namespace YAML
             rhs.rotation = node[5].as<float3>();
             rhs.scale = node[6].as<float3>();
             rhs.top = node[7].as<float>();
-            //rhs.attached = node[8].as<bool>(); // TODO: After save the scene add this line
+            rhs.attached = node[8].as<bool>();
 
             return true;
         }
@@ -484,7 +484,7 @@ namespace YAML
 
         static bool decode(const Node& node, Hachiko::ParticleSystem::ParticleProperties& rhs)
         {
-            if (!node.IsSequence() || node.size() != 5) // After save the scene increment to 6
+            if (!node.IsSequence() || node.size() != 6)
             {
                 return false;
             }
@@ -494,7 +494,7 @@ namespace YAML
             rhs.render_mode = static_cast<Hachiko::ParticleSystem::ParticleRenderMode>(node[2].as<int>());
             rhs.orientate_to_direction = node[3].as<bool>();
             rhs.stretch = node[4].as<Hachiko::ParticleSystem::VariableTypeProperty>();
-            //rhs.draw = node[5].as<bool>(); // TODO: After save the scene add this line
+            rhs.draw = node[5].as<bool>();
             return true;
         }
     };
